2889809ef779c17b2c08f4e87dc0ed8d1611f24c
sem15 <stevenemaxwell@gmail.com>
2026-03-23 09:39:55 -0400

    update multiComp

--- src/components/pages/lesson2/slide29.vue
+++ src/components/pages/lesson2/slide29.vue
@@ -1,49 +1,78 @@
 <template>
   <div class="page" id="content">
-    <div class="title">Scope</div>
-    <div class="body-text">
-      <p>
-        THIS IS A MULTIPLE CHOICE SLIDE
-      </p>
+    <div class="title">Check on Learning - LSA #1</div>
+    <div class="body-text">Complete a Check on Learning to summarize the Learning Activity and assess your understanding of the material presented in this LSA.</div>
+    <div class="content-box">
+
+      <div class="question">
+        <multi-comp :question="'Q1. Nonappropriated fund instrumentalities (NAFIs) and their programs are classified into how many groups?'" :choices="[
+        '3',
+        '6',
+        '2',
+        '4']" :correctAnswer="'6'" />
+      </div>
+      <div class="question">
+        <multi-comp :question="'Q2. True or False:  Military personnel normally operate tactical field exchanges.'" :choices="[
+        'True',
+        'False']" :correctAnswer="'True'" />
+      </div>
     </div>
+
+
   </div>
 
   <!-- NOTES -->
   <Teleport to="#notes-comp">
     <p>
-      N/A
+      Q1. Reference AR 215-1, Chapter 3. Section I.<br>
+      Q2. Reference FM 4-1, Para. 4-127.
     </p>
   </Teleport>
 
 </template>
 
 <script>
+import multiComp from '@/components/questions/multiComp.vue'
 
 export default {
   name: 'slideOne',
   mounted() {
     this.$store.commit('pageloaded')
-  }
+  },
+  components: {
+    multiComp,
+  },
 }
 </script>
 <style scoped>
 .page {
   overflow: scroll;
 }
-.graphic {
-  width: 40%;
+
+.title {
+  /* border: 1px solid red; */
+  margin-bottom: 20px;
 }
 .body-text {
-  text-align: left;
+  font-size: 1.1rem;
 }
-li {
-  margin-bottom: 15px;
+.graphic {
+  width: 75%;
 }
-.slide-container {
-  display: flex;
-  flex-direction: row;
-  gap: 10%;
+.body-text {
+  text-align: center;
+  margin-bottom: 30px;
+}
+.content-box {
+  height: 90%;
   width: 100%;
-  /* border: 1px solid red; */
+  display: flex;
+  flex-direction: column;
+  /* border: 1px solid blue; */
+}
+.question {
+  height: 100%;
+  width: 100%;
+  margin-bottom: 20px;
 }
 </style>
--- src/components/pages/lesson2/slide31.vue
+++ src/components/pages/lesson2/slide31.vue
@@ -1,49 +1,80 @@
 <template>
   <div class="page" id="content">
-    <div class="title">Scope</div>
-    <div class="body-text">
-      <p>
-        THIS IS A MULTIPLE CHOICE SLIDE
-      </p>
+    <div class="title">Check on Learning - LSA #1</div>
+    <div class="body-text">Complete a Check on Learning to summarize the Learning Activity and assess your understanding of the material presented in this LSA.</div>
+    <div class="content-box">
+
+      <div class="question">
+        <multi-comp :question="'Q1. What are the funding categories within the nonappropriated fund instrumentalities (NAFIs)?'" :choices="[
+        'Mission Sustaining Programs',
+        'Community Support Programs',
+        'Revenue Generating Programs',
+        'All of the above.',]" :correctAnswer="'All of the above.'" />
+      </div>
+      <div class="question">
+        <multi-comp :question="'Q2. Who provides strategic support to the force for MWR services?'" :choices="[
+        'Army G-1',
+        'ASCC G-1/AG',
+        'Installation Management Command (IMCOM)',
+        'Theater Personnel Action Center']" :correctAnswer="'Installation Management Command (IMCOM)'" />
+      </div>
     </div>
+
+
   </div>
 
   <!-- NOTES -->
   <Teleport to="#notes-comp">
     <p>
-      N/A
+      Q1. Reference: AR 215-1, Figure 3-1<br>
+      Q2. Reference: ATP 1-0.1, Para. 2-2.
     </p>
   </Teleport>
 
 </template>
 
 <script>
+import multiComp from '@/components/questions/multiComp.vue'
 
 export default {
   name: 'slideOne',
   mounted() {
     this.$store.commit('pageloaded')
-  }
+  },
+  components: {
+    multiComp,
+  },
 }
 </script>
 <style scoped>
 .page {
   overflow: scroll;
 }
-.graphic {
-  width: 40%;
+
+.title {
+  /* border: 1px solid red; */
+  margin-bottom: 20px;
 }
 .body-text {
-  text-align: left;
+  font-size: 1.1rem;
 }
-li {
-  margin-bottom: 15px;
+.graphic {
+  width: 75%;
 }
-.slide-container {
-  display: flex;
-  flex-direction: row;
-  gap: 10%;
+.body-text {
+  text-align: center;
+  margin-bottom: 30px;
+}
+.content-box {
+  height: 90%;
   width: 100%;
-  /* border: 1px solid red; */
+  display: flex;
+  flex-direction: column;
+  /* border: 1px solid blue; */
+}
+.question {
+  height: 100%;
+  width: 100%;
+  margin-bottom: 20px;
 }
 </style>
